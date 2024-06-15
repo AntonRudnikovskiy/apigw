d263b63b6b6ca87ff0c8b32efacedc1c4089dc83
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-15 16:08:15 +0300

    jib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.1.0"
+    id("com.google.cloud.tools.jib") version "3.1.0"
 }
 
 group = "sentinelguard"
-version = "1.0-SNAPSHOT"
+version = "1.0-apigw-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -16,4 +18,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator:3.1.5")
     implementation("io.micrometer:micrometer-tracing-bridge-brave:1.0.2")
     implementation("io.zipkin.reporter2:zipkin-reporter-brave:2.16.3")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.0")
+}
+
+jib {
+    from {
+        image = "eclipse-temurin:17"
+    }
+    to {
+        image = "docker.io/unkindledone/unkindledone-repository"
+        tags = setOf(project.version.toString())
+        auth {
+            username = findProperty("dockerHubUsername") as String?
+            password = findProperty("dockerHubPassword") as String?
+        }
+    }
+    container {
+        mainClass = "sentinelguard.apigw.ApiGWApplication"
+    }
 }
